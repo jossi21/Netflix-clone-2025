5208f70579b6f5383ec86fc78f20a4cb851d03f9
jossi21 <ahushabush4@gmail.com>
2025-03-26 01:38:06 +0300

    try

--- netflex-clone/src/components/Footer/Footer.jsx
+++ netflex-clone/src/components/Footer/Footer.jsx
@@ -8,37 +8,52 @@ const Footer = () => {
     <>
       <div className="footer_outer_container">
         <div className="footer_inner_container">
-          <div className="footer_icons"></div>
+          <div className="footer_icons">
+            <div className="">
+              <FacebookOutlinedIcon />
+            </div>
+            <div className="">
+              <InstagramIcon />
+            </div>
+            <div className="">
+              <YouTubeIcon />
+            </div>
+          </div>
           <div className="footer_data">
-            <FacebookOutlinedIcon />
-            <InstagramIcon />
-            <YouTubeIcon />
-            <div>
+            <div className="footer_left_side">
+              <ul>
+                <li>Audio and Subtitle</li>
+                <li>Media Center</li>
+                <li>Privacy</li>
+              </ul>
+            </div>
+            <div className="footer_middle_1">
               <ul>
                 <li>Audio Description</li>
-                <li>Investor Relation</li>
-                <li>Legal Notice</li>
+                <li>Investor Relations</li>
+                <li>Legal relations</li>
               </ul>
             </div>
-            <div>
+            <div className="footer_middle_2">
               <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                <li>Corporative Information</li>
               </ul>
             </div>
-            <div>
+            <div className="footer_right_side">
               <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Gift Cards</li>
+
+                <li>Terms of Use</li>
+                <li>Contact Us</li>
               </ul>
             </div>
           </div>
-          <div className="footer_icons"></div>
-          <div className="footer_icons"></div>
-          <div className="footer_icons"></div>
-          <div className="footer_icons"></div>
+          <div className="footer_copy_right">
+            <div className="service_code">Service code</div>
+            <div className="">&copy; 2025 netflix, inc.</div>
+          </div>
         </div>
       </div>
     </>
